fbf5a31f2d410f4b301a972056d173b6e07ae6ca
bswlkh0310 <hhhello0507@gmail.com>
2023-09-03 12:51:35 +0900

    server image upload

--- 15_discord_clone/discord/components/modals/initial-modal.tsx
+++ 15_discord_clone/discord/components/modals/initial-modal.tsx
@@ -3,6 +3,8 @@
 import * as z from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from "react-hook-form";
+import { useState, useEffect } from 'react';
+
 import {
     Dialog,
     DialogContent,
@@ -23,8 +25,7 @@ import {
 
 import { Input } from '../ui/input';
 import { Button } from '../ui/button';
-
-import { useState, useEffect } from 'react';
+import { FileUpload } from '@/components/file-upload';
 
 const formSchema = z.object({
     name: z.string().min(1, {
@@ -71,7 +72,22 @@ const InitialModal = () => {
                     <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
                         <div className='space-y-8 px-6'>
                             <div className='flex items-center justify-center text-center'>
-                                이미지 업로드하기
+                                <FormField
+                                control={form.control}
+                                name="imageUrl"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormControl>
+                                            <FileUpload
+                                            endpoint="serverImage"
+                                            value={field.value}
+                                            onChange={field.onChange}
+                                            />
+                                        </FormControl>
+                                    </FormItem>
+                                )}>
+
+                                </FormField>
                             </div>
                             <FormField
                             control={form.control}
